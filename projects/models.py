f28ed7ca901ec39217aee986f091d511ecfe673a
Sarah Vitvitskiy <sarah.a.vitvitskiy@gmail.com>
2023-11-13 07:05:41 -0800

    Initial Commit

---
+++ projects/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=100)
+    repository = models.CharField(max_length=100, blank=True)
+    image = models.FileField(upload_to="project_images/", blank=True)
+
+    def __str__(self):
+        return self.title
