771cf787bbd493d297158e9bdf0b95465aa8f30f
Sarah Vitvitskiy <sarah.a.vitvitskiy@gmail.com>
2023-11-13 10:15:02 -0800

    added whitenoise to the installed apps, removed secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3vin%v)2fc4#5@2fa9@abe&cx39trqyx@zxonrsfxa8$upu$b-"
+# SECRET_KEY = "django-insecure-3vin%v)2fc4#5@2fa9@abe&cx39trqyx@zxonrsfxa8$upu$b-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
