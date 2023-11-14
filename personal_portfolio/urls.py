084531609755db18223cf51569c10a34259ecea6
Sarah Vitvitskiy <sarah.a.vitvitskiy@gmail.com>
2023-11-14 12:07:24 -0800

    reverted change of project url

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("projects.urls")),
+    path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
